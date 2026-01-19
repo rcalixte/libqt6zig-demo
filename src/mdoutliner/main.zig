fa17a30a4d5f564afc381d19abff1104c99f5f8f
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-01-19 16:19:06 -0500

    mdoutliner: Switch to unmanaged maps

--- src/mdoutliner/main.zig
+++ src/mdoutliner/main.zig
@@ -31,8 +31,8 @@ const allocator = std.heap.smp_allocator;
 
 const lineNumberRole = qnamespace_enums.ItemDataRole.UserRole + 1;
 
-const AppTabMap = std.AutoHashMap(?*anyopaque, *AppTab);
-const AppWindowMap = std.AutoHashMap(?*anyopaque, *AppWindow);
+const AppTabMap = std.AutoHashMapUnmanaged(?*anyopaque, *AppTab);
+const AppWindowMap = std.AutoHashMapUnmanaged(?*anyopaque, *AppWindow);
 
 var app_tab_map: AppTabMap = undefined;
 var app_window_tab_map: AppWindowMap = undefined;
@@ -246,8 +246,8 @@ pub fn NewAppTab() !*AppTab {
     qlistwidget.OnCurrentItemChanged(ret.outline, AppTab.handleJumpToBookmark);
 
     ret.textArea = qtextedit.New(ret.tab);
-    try app_tab_map.put(ret.textArea, ret);
-    try app_tab_map.put(ret.outline, ret);
+    try app_tab_map.put(allocator, ret.textArea, ret);
+    try app_tab_map.put(allocator, ret.outline, ret);
 
     qtextedit.OnTextChanged(ret.textArea, AppTab.handleTextChanged);
     qsplitter.AddWidget(panes, ret.textArea);
@@ -435,7 +435,7 @@ pub fn NewAppWindow() !*AppWindow {
     const sampleContent = @embedFile("README.md");
     AppWindow.createTabWithContents(ret, "README.md", sampleContent);
 
-    try app_window_tab_map.put(ret.tabs, ret);
+    try app_window_tab_map.put(allocator, ret.tabs, ret);
     main_window = ret;
 
     return ret;
@@ -465,8 +465,8 @@ pub fn main() !void {
         }
     }
 
-    app_tab_map = AppTabMap.init(allocator);
-    app_window_tab_map = AppWindowMap.init(allocator);
+    app_tab_map = .empty;
+    app_window_tab_map = .empty;
 
     defer {
         var processed_tabs: std.AutoHashMapUnmanaged(*AppTab, void) = .empty;
@@ -481,8 +481,8 @@ pub fn main() !void {
                 allocator.destroy(apptab);
             }
         }
-        app_tab_map.deinit();
-        app_window_tab_map.deinit();
+        app_tab_map.deinit(allocator);
+        app_window_tab_map.deinit(allocator);
     }
 
     const app = try NewAppWindow();

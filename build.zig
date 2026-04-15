68515662c4a496637637f02b39c8c5dbc8deca73
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-04-15 16:47:48 -0400

    build.zig: Update to Zig 0.16

--- build.zig
+++ build.zig
@@ -4,18 +4,16 @@ const host_os = @import("builtin").os.tag;
 var qt_dir: []const u8 = "";
 
 pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
     const extra_paths = b.option([]const []const u8, "extra-paths", "Extra library header and include search paths") orelse &.{};
-
-    var optimize = b.standardOptimizeOption(.{});
-    if (optimize == .Debug) optimize = .ReleaseFast;
 
     const is_macos = target.result.os.tag == .macos or host_os == .macos;
     const is_windows = target.result.os.tag == .windows or host_os == .windows;
 
     if (is_windows) {
         qt_dir = b.option([]const u8, "QTDIR", "The directory where Qt is installed") orelse win_root;
-        std.fs.cwd().access(qt_dir, .{}) catch {
+        std.Io.Dir.cwd().access(b.graph.io, qt_dir, .{}) catch {
             std.log.err("QTDIR '{s}' does not exist\n", .{qt_dir});
             return error.QTDIRNotFound;
         };
@@ -36,7 +34,7 @@ pub fn build(b: *std.Build) !void {
 
     const qt6zig = b.dependency("libqt6zig", .{
         .target = target,
-        .optimize = if (optimize == .Debug) .ReleaseFast else optimize,
+        .optimize = optimize,
         .@"extra-paths" = extra_paths,
     });
 
@@ -54,17 +52,16 @@ pub fn build(b: *std.Build) !void {
     // Link Qt system libraries
     const sub_paths = [_][]const u8{ "/bin", "/lib", "" };
 
-    for (extra_paths) |path| {
+    for (extra_paths) |path|
         for (sub_paths) |sub_path| {
             const extra_path = b.fmt("{s}{s}", .{ path, sub_path });
-            std.fs.cwd().access(extra_path, .{}) catch {
+            std.Io.Dir.cwd().access(b.graph.io, extra_path, .{}) catch {
                 continue;
             };
             exe.root_module.addLibraryPath(.{ .cwd_relative = extra_path });
             if (is_macos)
                 exe.root_module.addFrameworkPath(.{ .cwd_relative = extra_path });
-        }
-    }
+        };
 
     if (is_bsd_host)
         exe.root_module.addLibraryPath(.{ .cwd_relative = "/usr/local/lib/qt6" });
@@ -75,32 +72,26 @@ pub fn build(b: *std.Build) !void {
     if (is_windows) {
         const win_bin_dir = win_root ++ "/bin";
         var ok_bin_dir = true;
-        std.fs.cwd().access(win_bin_dir, .{}) catch {
+        std.Io.Dir.cwd().access(b.graph.io, win_bin_dir, .{}) catch {
             if (extra_paths.len == 0) {
                 std.log.err("'{s}' either does not exist or does not have a 'bin' subdirectory and no extra paths were provided\n", .{win_root});
                 return error.WinQtBinDirNotFound;
-            } else {
-                ok_bin_dir = false;
-            }
+            } else ok_bin_dir = false;
         };
-        if (ok_bin_dir) {
+        if (ok_bin_dir)
             exe.root_module.addLibraryPath(.{ .cwd_relative = win_bin_dir });
-        }
     }
 
-    for (base_libs) |lib| {
-        if (is_macos) {
-            exe.root_module.linkFramework(lib, .{});
-        } else {
+    for (base_libs) |lib|
+        if (is_macos)
+            exe.root_module.linkFramework(lib, .{})
+        else
             exe.root_module.linkSystemLibrary(lib, .{});
-        }
-    }
 
-    if (is_linux_host) {
+    if (host_os == .linux) {
         exe.root_module.link_libcpp = false;
 
-        const result = try std.process.Child.run(.{
-            .allocator = b.allocator,
+        const result = try std.process.run(b.allocator, b.graph.io, .{
             .argv = &.{ "gcc", "--print-file-name=libstdc++.so.6" },
         });
         exe.root_module.addObjectFile(.{
@@ -111,9 +102,12 @@ pub fn build(b: *std.Build) !void {
     }
 
     // Link libqt6zig static libraries
-    for (qt_libraries) |lib| {
+    for (qt_libraries) |lib|
         exe.root_module.linkLibrary(qt6zig.artifact(lib));
-    }
+
+    if (optimize == .Debug)
+        for (debug_libraries) |lib|
+            exe.root_module.linkLibrary(qt6zig.artifact(lib));
 
     // Create a check step
     const check_step = b.step("check", "Check the build without generating an executable");
@@ -140,7 +134,7 @@ pub fn build(b: *std.Build) !void {
         for (win_libs) |lib| {
             const bin_path = b.fmt("bin/{s}.dll", .{lib});
             const dll_path = b.fmt("{s}/{s}", .{ qt_dir, bin_path });
-            std.fs.cwd().access(dll_path, .{}) catch {
+            std.Io.Dir.cwd().access(b.graph.io, dll_path, .{}) catch {
                 continue;
             };
             const install_win_dll = b.addInstallFile(.{ .cwd_relative = dll_path }, bin_path);
@@ -148,7 +142,7 @@ pub fn build(b: *std.Build) !void {
         }
 
         const plugins_path = b.fmt("{s}/plugins", .{qt_dir});
-        std.fs.cwd().access(plugins_path, .{}) catch {
+        std.Io.Dir.cwd().access(b.graph.io, plugins_path, .{}) catch {
             std.log.err("Plugins path '{s}' does not exist\n", .{plugins_path});
             return error.PluginsPathNotFound;
         };
@@ -163,11 +157,6 @@ pub fn build(b: *std.Build) !void {
 
 const is_bsd_host = switch (host_os) {
     .dragonfly, .freebsd, .netbsd, .openbsd => true,
-    else => false,
-};
-
-const is_linux_host = switch (host_os) {
-    .linux => true,
     else => false,
 };
 
@@ -198,4 +187,9 @@ const qt_libraries = [_][]const u8{
     "qtranslator",
     "qvariant",
     "qwidget",
+};
+
+const debug_libraries = [_][]const u8{
+    "qlabel",
+    "qstatusbar",
 };
